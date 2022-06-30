4405ca16b1fe30aa23c58e931087c1bf01e7e3e2
Felix Engelhardt <felix.engelhardt@eidu.com>
2022-06-30 17:52:52 +0200

    🔥 JsonUtil: remove dead code - FUNZA-33

--- src/main/kotlin/com/eidu/content/learningpackages/util/JsonUtil.kt
+++ src/main/kotlin/com/eidu/content/learningpackages/util/JsonUtil.kt
@@ -1,13 +1,8 @@
 package com.eidu.content.learningpackages.util
 
-import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.serializer
 
 val json = Json {
     encodeDefaults = true
     ignoreUnknownKeys = true
 }
-
-@OptIn(InternalSerializationApi::class)
-inline fun <reified T : Any> ByteArray.parseJson() = json.decodeFromString(T::class.serializer(), decodeToString())
