77aba87da874b0f953933483a9f522aee53da847
Felix Engelhardt <felix.engelhardt@eidu.com>
2023-04-18 17:36:14 +0200

    ⚡️ TagsSerializer: don't use reflection to obtain internal serializer

--- src/main/kotlin/com/eidu/content/learningpackages/util/TagsSerializer.kt
+++ src/main/kotlin/com/eidu/content/learningpackages/util/TagsSerializer.kt
@@ -1,13 +1,16 @@
 package com.eidu.content.learningpackages.util
 
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.builtins.SetSerializer
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.serializer
 
 object TagsSerializer : KSerializer<Map<String, Set<String>>> {
-    private val surrogateSerializer = serializer<Map<String, Set<String>>>()
+    private val surrogateSerializer = MapSerializer(String.serializer(), SetSerializer(String.serializer()))
+
     override val descriptor: SerialDescriptor = surrogateSerializer.descriptor
 
     override fun serialize(encoder: Encoder, value: Map<String, Set<String>>) =
